c357a467768a1c31b03f5e282c26cb5cd189ffb3
Jisma Varghese <jismavarghese34@gmail.com>
2025-07-15 07:10:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddQuote from './components/AddQuote';
-import SearchQuote from './components/SearchQuote';
-import DeleteQuote from './components/DeleteQuote';
+
 import ViewQuote from './components/ViewQuote';
 
 function App() {
@@ -11,9 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<AddQuote />} />
-          <Route path="/search" element={<SearchQuote />} />
-           <Route path="/delete" element={<DeleteQuote />} />
-
+        
           <Route path="/view" element={<ViewQuote />} />
         </Routes>
       </BrowserRouter>
